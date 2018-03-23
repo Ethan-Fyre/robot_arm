fca47efeeb6ffd9594007dca061cc672d1dc47d4
Ethan-Fyre <jasayles@anderson.edu>
2018-03-23 16:25:17 -0400

    Program is fully functional.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# robot_arm
-Path optimization for a robotic arm
+# Path Planning
+This project is an experiment in applying local searching techniques to perform optimal and suboptimal movement planning. rid where each grid element is a unit square containingspeed of 0. The path allows 8-directional movement, where each step is from the center of one square to the center of the next square.
+## Getting Started
+
+This program is callable from the command line, and has the following format:
+```
+python3 project2.py <grid filename> <start coordinates> <stop coordinates>
+```
+There is an optional argument '--alg' which defaults to 'a_star', but can be set to 'ucs' for uniform cost search, and 'bfs' for breadth first search. Entering in any other value will just use A* search. Grid is a csv file, and start, stop are of the form r,c specifying the row and column.
+
+## Properties of search algorithms
+
+The first search is the breadth-first search (BFS). The BFS is complete for this problem, as the branching factor is finite (3 for corners, 5 for edges, 8 for interior pieces) and the goal will be reachable eventually. BFS is only optional in the case that all the grid locations have the same speed; otherwise it is not guarenteed to be optimal. The time to find a solution is O(b^d), where b is the branching factor, and d is the depth of the solution. The space complexity is a factor of the time complexity, and is O(b^d) as well. The second search is the uniform-cost search (UCS). The UCS is optimal and complete for the grid problem, as it explores the nodes with the lowest path cost. However, the worst-case time and space complexity for the UCS is O(b^(1+floor(C* / e))) where C* is the cost of the optimal solution, and every action costs at least e. The final search is the A* which is guarenteed to be optimal for an admissable heuristic function. Given that this problem is in finding a path, the straight line distance was uesd as the hueristic function.The drawback to A* lies in the exponential complexity. The complexity is defined as O(b^(h* - h)) where h* is the actual cost of getting from the inital state to the goal state.
+
+## Calculation of step cost
+The derivation of the step costs lies in geometry and unit analysis. The squares are unit squares, and all actions move from the center of one square to the center of the next square. As such the time spent in one square while moving horizontally/vertically is 1/2 distance. The speed at this time is the value in the grid location, and is distance/time. In order to get the time to leave one square in a horizontal/vertical motion we must multiply the distance by the inverse of the speed. This results in the equation 1/2 * 1/speed1, where speed1 is the speed of the start location. By the same reasoning the time through  the second square is 1/2 * 1/speed2. Therefore, the total time to transition from the center of one square to the center of another is 1/2 * (1/speedq + 1/speed2) through factoring. However, this equation does not account for diagonal movement. The diagonal distance to leave a square can be found through geometry to be 1/sqrt(2). Using the reasoning as the previous section, the time to move diagonally is 1/sqrt(2) * (1/speed1 + 1/speed2).
+### Prerequisites
+This program requires the following libraries: argparse, numpy, collections, math,  random, sys, bisect, operator,
+os.path, functools, itertools.
+
+Most of these imports derive from the AIMA search.py and utils.py files that are used in this program.
+
--- robot_arm.py
+++ robot_arm.py
@@ -16,18 +16,52 @@ import random
 class ArmClimb(aima.Problem):
     """Subclass of Problem that pertains to this project"""
 
-    def __init__(self, arms, polylist, initial, goal):
+    def __init__(self, arms, polylist, initial, limits, goal):
         """Constructor for the class"""
         aima.Problem.__init__(self, initial, goal)
         self.arms = arms
         self.polylist = polylist
+        self.limits = limits
+
+    def random_start(self, state):
+        """Function ensures the arm will not start inside an obstacle"""
+        thetalist = [i[-1] * np.pi / 180 for i in state]
+        x = [0] * (len(state) + 1)
+        y = [0] * (len(state) + 1)
+        thetasum = np.cumsum(thetalist)
+        for i in range(len(state)):
+            x[i + 1] = self.arms[i] * np.cos(thetasum[i])
+            y[i + 1] = self.arms[i] * np.sin(thetasum[i])
+        x = np.cumsum(x)
+        y = np.cumsum(y)
+        xypairs = [[] for _ in range(len(x) - 1)]
+        for i in range(len(x) - 1):
+            xypairs[i] = [(x[i], y[i]), (x[i + 1], y[i + 1])]
+        lines = [LineString(i) for i in xypairs]
+        for i in lines:
+            for j in self.polylist:
+                if j.intersects(i):
+                    new = [[random.randint(limits[i][0], limits[i][1])] for i in range(len(state))]
+                    return self.random_start(new)
+        return state
 
     def actions(self, state):
         """ Function to specify the actions given a state"""
         acts = [np.zeros(len(state)) for _ in range(2*len(state))]
-        for i in range(len(state)):
-            acts[2*i][i] = 5
-            acts[2*i + 1][i] = -5
+        x = -1 * self.value(state)
+        if x < 3:
+            for i in range(len(state)):
+                acts[2 * i][i] = x * 5
+                acts[2 * i + 1][i] = -x * 5
+        else:
+            for i in range(len(state)):
+                acts[2*i][i] = 10
+                acts[2*i + 1][i] = -10
+        for i in acts:
+            for j in range(len(i)):
+                if not self.limits[j][0] <= state[j][-1] <= self.limits[j][1]:
+                    acts.remove(i)
+                    break
         return acts
 
     def result(self, state, action):
@@ -55,6 +89,7 @@ class ArmClimb(aima.Problem):
         return new_state
 
     def value(self, state):
+        """Function to calculate the value to be maximized by the local search algorithms"""
         thetalist = [i[-1] * np.pi / 180 for i in state]
         x = [0] * (len(state) + 1)
         y = [0] * (len(state) + 1)
@@ -67,12 +102,13 @@ class ArmClimb(aima.Problem):
         val = -1 * np.sqrt((xee - self.goal[0]) ** 2 + (yee - self.goal[1]) ** 2)
         return val
 
-    def sched(self, k=20, lam=.01, limit=500):
+    def sched(self, k=1, lam=.01, limit=100):
         """One possible schedule function for simulated annealing"""
         return lambda t: (k * np.exp(-lam * t) if t < limit else 0)
 
 
 def graph(l, thetas):
+    """Function to return a list of xs and ys given lengths and angles"""
     thetas = [i * np.pi / 180 for i in thetas]
     x = [0] * (len(thetas) + 1)
     y = [0] * (len(thetas) + 1)
@@ -85,18 +121,45 @@ def graph(l, thetas):
     return x, y
 
 
-def local_beam_search(problem, k=5, tol=-.01):
+def hill_climbing_with_tol(problem, tol=.01):
     """From the initial node, keep choosing the neighbor with highest value,
-    stopping when no neighbor is better. [Figure 4.2]"""
-    current = [aima.Node([[random.randint(-90, 90)] for _ in range(len(problem.arms))]) for _ in range(k)]
-    inwall = [True] * len(current)
-    while any(inwall):
-        for i in current:
-            if all([j == i for j in i.expand(problem)]):
-                current.append(aima.Node([[random.randint(-90, 90)] for _ in range(len(problem.arms))]))
-                current.remove(i)
-            else:
-                inwall[current.index(i)] = False
+    stopping when no neighbor is better. (Slightly modified version of the
+    aima hill_climbing function)"""
+    current = aima.Node(problem.random_start(problem.initial))
+    while True:
+        neighbors = current.expand(problem)
+        if not neighbors:
+            break
+        neighbor = aima.argmax_random_tie(neighbors,
+                                     key=lambda node: problem.value(node.state))
+        if problem.value(neighbor.state) <= problem.value(current.state) or problem.value(current.state) > -1*tol:
+            break
+        current = neighbor
+    return current.state
+
+
+def simulated_annealing_with_tol(problem, tol=.01):
+    """Modified version of the aimi simulated annealing function that takes
+    a problem and a tolerance"""
+    schedule = problem.sched()
+    current = aima.Node(problem.random_start(problem.initial))
+    for t in range(sys.maxsize):
+        T = schedule(t)
+        if T == 0 or problem.value(current.state) > -1 * tol:
+            return current.state
+        neighbors = current.expand(problem)
+        if not neighbors:
+            return current.state
+        next = random.choice(neighbors)
+        delta_e = problem.value(next.state) - problem.value(current.state)
+        if delta_e > 0 or aima.probability(np.exp(delta_e / T)):
+            current = next
+
+
+def local_beam_search(problem, k=5, tol=.01):
+    """From the initial k nodes, keep choosing the k neighbors with highest value,
+    stopping when no neighbors are better (or it is withing the tolerance)."""
+    current = [aima.Node(problem.random_start([[random.randint(problem.limits[i][0], problem.limits[i][1])] for i in range(len(problem.arms))])) for _ in range(k)]
     while True:
         neighbors = []
         for i in current:
@@ -109,12 +172,13 @@ def local_beam_search(problem, k=5, tol=-.01):
                                      key=lambda node: problem.value(node.state))
             neighbor.append(newneighbor)
             neighbors.remove(neighbor[-1])
+        closer = []
         for i in neighbor:
-            if problem.value(i.state) > tol:
+            if problem.value(i.state) > -1 * tol:
                 return i.state
-            closer = []
             for j in current:
                 closer.append(problem.value(j.state) >= problem.value(i.state))
+
         if all(closer):
             break
         current = neighbor
@@ -122,19 +186,24 @@ def local_beam_search(problem, k=5, tol=-.01):
                                      key=lambda node: problem.value(node.state)).state
 
 
-def main(lens ,limits, goal, joints, obstacles, alg):
+def main(lens, limits, goal, joints, obstacles, alg, slow):
     """Default function to be called when the program executes."""
-    robot_arm = ArmClimb(lens, obstacles, joints, goal)
+    print(limits)
+    robot_arm = ArmClimb(lens, obstacles, joints, limits, goal)
     if alg == "lbs":
+        print("Now beginning Local Beam Search:")
         newstate = local_beam_search(robot_arm)
-    elif alg == "sta":
-        newstate = aima.simulated_annealing(robot_arm, robot_arm.sched())
+    elif alg == "sia":
+        print("Now beginning Simulated Annealing:")
+        newstate = simulated_annealing_with_tol(robot_arm)
     elif alg == "rar":
-        newstate = aima.hill_climbing(robot_arm)
+        print("Now beginning Random Restart:")
+        newstate = hill_climbing_with_tol(robot_arm)
     else:
-        print("algorithm is not implemented. Using local beam search instead")
+        print("Algorithm is not implemented. Beginning Local Beam Search instead:")
         alg = "lbs"
         newstate = local_beam_search(robot_arm)
+
     thetastart = [i[0] for i in newstate]
     xdata, ydata = graph(robot_arm.arms, thetastart)
     plt.show()
@@ -146,74 +215,67 @@ def main(lens ,limits, goal, joints, obstacles, alg):
     axes.set_xlim(-1 * np.sum(robot_arm.arms), np.sum(robot_arm.arms))
     axes.set_ylim(-1 * np.sum(robot_arm.arms), np.sum(robot_arm.arms))
     line, = axes.plot(xdata, ydata, 'b-')
+    elbows, = axes.plot(xdata, ydata, 'b.')
     while True:
         for i in range(len(newstate[0])):
             thetas = [j[i] for j in newstate]
             xdata, ydata = graph(robot_arm.arms, thetas)
             line.set_xdata(xdata)
             line.set_ydata(ydata)
+            elbows.set_xdata(xdata)
+            elbows.set_ydata(ydata)
             plt.draw()
-            plt.pause(0.0001)
+            if slow:
+                plt.pause(.1)
+            else:
+                plt.pause(0.0001)
         if alg == "rar":
-            if robot_arm.value(newstate) > -.05:
+            if robot_arm.value(newstate) > -.01:
                 break
-            start = [[random.randint(-90, 90)] for _ in range(len(robot_arm.arms))]
+            start = [[random.randint(limits[i][0], limits[i][1])] for i in range(len(robot_arm.arms))]
             robot_arm.initial = start
-            newstate = aima.hill_climbing(robot_arm)
+            newstate = hill_climbing_with_tol(robot_arm)
             plt.pause(.001)
         else:
             break
-    print("Final position angles are ",[i[-1] for i in newstate])
-    print("Value is ", robot_arm.value(newstate))
+    print("Final position angles are ", [i[-1] for i in newstate])
+    print("Distance is ", -1 * robot_arm.value(newstate))
     plt.show()
 
 
 if __name__ == '__main__':
-
     # Arguments for argparse.
     parser = argparse.ArgumentParser(description='')
-    argv = sys.argv[1:]
     parser.add_argument("arm_file", help="")
     parser.add_argument("goal", help="")
-    if "--joints" in argv:
-        i = argv.index("--joints")
-        argv2 = argv[:i] + argv[i+2:]
-        if "--alg" in argv2:
-            j = argv2.index("--alg")
-            argv3 = argv2[:j] + argv2[j + 2:]
-            parser.add_argument("ob_files", nargs=argparse.REMAINDER)
-            args, joints, alg = parser.parse_args(argv3), argv[i + 1], argv2[j + 1]
-        else:
-            parser.add_argument("ob_files", nargs=argparse.REMAINDER)
-            args, joints, alg = parser.parse_args(argv2), argv[i + 1], "lbs"
-    else:
-        if "--alg" in argv:
-            j = argv.index("--alg")
-            argv2 = argv[:j] + argv[j + 2:]
-            parser.add_argument("ob_files", nargs=argparse.REMAINDER)
-            args, joints, alg = parser.parse_args(argv2), [], argv[j + 1]
-        else:
-            parser.add_argument("ob_files", nargs=argparse.REMAINDER)
-            args, joints, alg = parser.parse_args(argv), [], "lbs"
+    parser.add_argument("--slow", action='store_true')
+    parser.add_argument("--joints", default=[])
+    parser.add_argument("--alg", default="lbs")
+    parser.add_argument("ob_files", nargs="+")
+    args = parser.parse_args(sys.argv[1:])
+
     lens = []
     limits = []
     obstacles = []
+    # read in obstacle data
     for i in args.ob_files:
         with open(i) as file:
             coords = []
             for data in file:
                 coords.append(tuple([int(i) for i in data.split(' ')]))
         obstacles.append(Polygon(coords))
-    with open(args.arm_file) as file:  # Use file to refer to the file object
+
+    # read in arm data
+    with open(args.arm_file) as file:
         for data in file:
             datalist = [i for i in data.split(' ')]
             lens.append(int(datalist[1]))
             limits.append((int(datalist[2]), int(datalist[3])))
     goal = [int(i) for i in args.goal.split(',')]
-    if not joints:
-        joints = [[random.randint(-90, 90)] for _ in range(len(lens))]
+    if not args.joints:
+        joints = [[random.randint(limits[i][0], limits[i][1])] for i in range(len(lens))]
     else:
-        joints = [[int(i)] for i in joints.split(',')]
-main(lens, limits, goal, joints, obstacles, alg)
+        joints = [[int(i)] for i in args.joints.split(',')]
+main(lens, limits, goal, joints, obstacles, args.alg, args.slow)
 
 
